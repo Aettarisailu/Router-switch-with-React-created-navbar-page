50cb278016fe9fdc43d0de0f62abbe8f02d32652
ananisai <ananisai9294@gmail.com>
2023-11-08 14:49:06 +0000

    Switch-Router/with-react

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="header-container">
+    <div className="logo-and-title-container">
+      <img
+        alt="wave"
+        className="logo"
+        src="https://res.cloudinary.com/drevfgyks/image/upload/v1694333811/20230213_232002_p6djmh.png"
+      />
+      <h1 className="nanisai">www.nanisai.com</h1>
+    </div>
+
+    <ul className="nav-items-list">
+      <li className="link-item">
+        <Link className="route-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
